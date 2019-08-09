f19cedade51680e5456dc71e15d34358a9f333de
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-09 22:55:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_arr)
+  
+  dwarves_arr.each_with_index do |dwarf, idx|
+    puts "#{idx + 1} #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
