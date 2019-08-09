0ef7ecc72980635d6b39b810c9fcd4eac97a9265
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-09 22:58:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(dwarves_arr)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |calls| "#{calls}.capitalize!"
 end
 
 def long_planeteer_calls# code an argument here
